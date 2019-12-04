b3e12982d74f88b1e9d2e783e684af3a008d6e51
Nikita <hrozhek@gmail.com>
2019-12-05 00:03:42 +0300

    attempt to resolve issue with github

--- src/com/github/hrozhek/Main.java
+++ src/com/github/hrozhek/Main.java
@@ -3,30 +3,25 @@ package com.github.hrozhek;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test x^2 + 3*x + 4:");
-        fullQuadraticEquation(1, 3, 4);
-        System.out.println("test x^2 + 3*x - 4:");
-        fullQuadraticEquation(1, 3, -4);
-        System.out.println("test -6*x^2 - 5*x - 1:");
-        fullQuadraticEquation(-6, -5, -1);
-        System.out.println("test x^2 = 9:");
-        fullQuadraticEquation(1, 0, - 9);
-        System.out.println("test x^2 - 4x + 4:");
-        fullQuadraticEquation(1, -4, 4);
+        ObjectOrientedQuadraticEquation testWithoutRoots = new ObjectOrientedQuadraticEquation(1, 3, 4);
+        ObjectOrientedQuadraticEquation testWithOneRoot = new ObjectOrientedQuadraticEquation(1, -4, 4);
+        ObjectOrientedQuadraticEquation testWithTwoRoots = new ObjectOrientedQuadraticEquation(-6, -5, -1);
+        testWithoutRoots.sayResult();
+        testWithOneRoot.sayResult();
+        testWithTwoRoots.sayResult();
     }
 
-    private static void fullQuadraticEquation(double a, double b, double c){
+    private static void fullQuadraticEquation(double a, double b, double c) {
         double discriminant = b * b - 4 * a * c;
         if (discriminant < 0) {
             System.out.println("Discriminant is less than 0, there is no roots");
-        }
-        else if (discriminant == 0) {
+        } else if (discriminant == 0) {
             System.out.println("x = " + (-b / (2 * a)));
-        }
-        else {
-            double x1 = (-b + Math.sqrt(discriminant))/ (2 * a);
-            double x2 = (-b - Math.sqrt(discriminant))/ (2 * a);
+        } else {
+            double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
             System.out.println("x1 = " + x1 + ", x2 = " + x2);
         }
     }
 }
+
